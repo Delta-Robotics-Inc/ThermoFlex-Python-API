ef81debfe2f29dbecb440e5b75e86fb6cddbd1db
mstep44 <marvin.stephens44@yahoo.com>
2024-11-03 15:00:35 -0600

    updated changes

--- python-serial/staging/src/thermoflex/__init__.py
+++
@@ -1,7 +0,0 @@
-from .controls import discover
-#from .network import nodenet
-#from .devices import node, muscle
-
-#my library is the brains of the operation. Everything happens here. 
-#make Markdown file
-#Getting Started, Markdown with commands, Basic tutorial
